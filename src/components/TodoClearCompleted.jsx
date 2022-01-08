95bce2b2fd956180bf0bb43558f2e4903afcc950
mrzalais <lejnieksedijs@gmail.com>
2022-01-08 18:14:46 +0200

    Episode 7 - More Todo Features

---
+++ src/components/TodoClearCompleted.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+TodoClearCompleted.propTypes = {
+  clearCompleted: PropTypes.func.isRequired,
+}
+
+function TodoClearCompleted(props) {
+  return (
+    <button
+      onClick={props.clearCompleted}
+      className="button"
+    >
+      Clear Completed
+    </button>
+  )
+}
+
+export default TodoClearCompleted;
